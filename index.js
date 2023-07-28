e7bb58bb96562842c8d3df9723aea032aca93fe5
unknown <princep4050@gmail.com>
2023-07-28 17:06:05 +0530

    index.js edited

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 app.get("/",(req,res)=>{
-    res.send("Hi Prince here");
+    res.send("Hi Prince here but this was fun!");
 });
 
 app.listen(4000,()=>{
